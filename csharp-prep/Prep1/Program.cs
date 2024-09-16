8d8bdbd0f782f268c66be43d37f4542335f68f3b
lifegatejustice <jus24002@byui.edu>
2024-09-16 16:13:27 +0100

    "Modified Program.cs to prompt user for first and last name, then print out a formatted greeting."

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is you firstname? ");
+        string firstname = Console.ReadLine();
+
+        Console.Write("What is you lastname? ");
+        string lastname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastname}, {firstname}, {lastname}");
     }
+
+    
 }
