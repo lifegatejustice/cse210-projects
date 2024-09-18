44c5685ff7d6b8c6bae6966eba152669e81b4d77
lifegatejustice <jus24002@byui.edu>
2024-09-18 13:36:40 +0100

    "Added List<int> and modified Main method to prompt user for input"

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,19 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        Console.Write("Enter a list of numbers, type 0 when finished.");
+        string userList = Console.ReadLine();
+        int input = int.Parse(userList);
+
+        while (input != 0)
+        {
+            Console.WriteLine("Enter a Number")
+        }
+        
     }
 }
