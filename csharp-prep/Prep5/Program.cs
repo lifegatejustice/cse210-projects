39e4267a9d6d283d01b9ac88dc45b6b73c43a9f2
lifegatejustice <jus24002@byui.edu>
2024-09-19 14:39:50 +0100

    "Updated Program.cs to add new methods and modify existing ones, including DisplayMessage, DisplayPersonalMessage, Main, and squareRoot."

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void DisplayMessage()
     {
-        Console.WriteLine("Hello world!");
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static void DisplayPersonalMessage(string userName)
+    {
+        Console.WriteLine($"Please enter your name: {userName}");
+    }
+
+    static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            squareRoot(number);
+        }
+
+    static void squareRoot(int first)
+    {
+        double squareRoot = Math.Sqrt(first);
+        Console.WriteLine("The square root of " + first + " is " + squareRoot);
     }
 }
