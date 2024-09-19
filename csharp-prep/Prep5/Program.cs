4c8940535a978d0bead9b73235912a45e91fcbe4
lifegatejustice <jus24002@byui.edu>
2024-09-19 15:41:52 +0100

    "Removed DisplayPersonalMessage method and its call, moved SquareRoot method call to the end of Main method"

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -6,29 +6,19 @@ class Program
     {
         Console.WriteLine("Welcome to the Program!");
     }
-
-    static void DisplayPersonalMessage(string userName)
-    {
-        Console.WriteLine($"Hello, {userName}! Welcome to the program.");
-    }
-
     static void Main(string[] args)
     {
-        // Call the DisplayMessage method
         DisplayMessage();
 
-        // Ask for the user's name
         Console.Write("Please enter your name: ");
         string userName = Console.ReadLine();
-        
-        // Call the DisplayPersonalMessage method with the user's name
-        DisplayPersonalMessage(userName);
 
-        // Ask for a number to calculate the square root
         Console.Write("Enter your favourite number: ");
         int number = Convert.ToInt32(Console.ReadLine());
 
-        SquareRoot(number, userName); 
+        SquareRoot(number, userName);
+    }
+
 
     static void SquareRoot(int number, string userName)
     {
