0efc103df117a9f2b42616281003ff8f05e6dca3
lifegatejustice <jus24002@byui.edu>
2024-09-23 13:36:15 +0100

    "Added Job object initialization in Main method"

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Reflection.Metadata;
+
+public class Job
+        {
+            public string _company = "Apple";
+            public string _jobTitle = "Software Engineer";
+            public string _startYear = "2024";
+            public string _endYear = "2025";
+            
+        }
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Apple";
+        job1._startYear= "";
+        
+        
+
         Console.WriteLine("Hello Learning02 World!");
     }
 }
+
+    
