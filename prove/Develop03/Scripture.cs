cb4911a4efd9084ac909711e26bd9b6bb2acb719
lifegatejustice <jus24002@byui.edu>
2024-10-03 12:47:12 +0100

    "Removed trailing whitespace from end of file."

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,7 @@
+using System;
+public class Scripture
+{
+    private Reference _refrence;
+    public List<Word> _words;
+
+}
---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Collections.Generic;
+
+public class Word
+{
+    private string _text;
+    private bool _isHidden;
+}
