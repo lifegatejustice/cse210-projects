4bb5255a4f7d265c6c65bd7a3035b13b2c7ed23f
lifegatejustice <jus24002@byui.edu>
2024-10-03 13:31:17 +0100

    "Added Reference class and modified Scripture class to include Reference object and word parsing functionality."

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -1,3 +1,10 @@
 using System;
 using System.Collections.Generic;
 
+public class Reference
+{
+    private string _book;
+    private int _chapter;
+    private int _verse;
+    private int _endVerse;
+}
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,7 +1,22 @@
 using System;
+using System.Collections.Generic
 public class Scripture
 {
     private Reference _refrence;
     public List<Word> _words;
+
+    public Scripture(Reference _reference, string text)
+    {
+        _refrence = _reference;
+        _words = new List<Word>();
+
+        string[] wordsArray = text.Split(' ');
+        foreach (string word in wordsArray)
+        {
+            _words.Add(new Word(word));
+        }
+
+
+    }
 
 }
