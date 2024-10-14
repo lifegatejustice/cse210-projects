672882aa421a5f0f5ffdbb71afae2c3ed923a56d
lifegatejustice <lifegatedetom@gmail.com>
2024-10-14 15:43:16 +0100

    "Added instance creation and method call to Main method in Program.cs"

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        //Create New Instance of the class
+        Assignment assignment = new Assignment("John Doe", "Mathematics");
+        Console.WriteLine(assignment.GetSummary());
     }
 }
