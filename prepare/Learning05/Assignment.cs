21d09d9b5e3590351248c33eee403595dcd441ce
lifegatejustice <lifegatedetom@gmail.com>
2024-10-14 13:57:05 +0100

    "Removed Console.WriteLine statement in Main method, replaced with empty statement."

---
+++ prepare/Learning05/Assignment.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+
+public class Assignment
+{
+    private string _studentName;
+    private string _topic;
+
+    public Assignment(string  studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+}
