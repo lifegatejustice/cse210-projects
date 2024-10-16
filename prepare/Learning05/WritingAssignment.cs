8c24170cce84f6c845e38644b0626b7e1b6357a2
lifegatejustice <lifegatedetom@gmail.com>
2024-10-16 03:26:08 -0700

    finished WritingAssignment class

---
+++ prepare/Learning05/WritingAssignment.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+
+public class WritingAssignment : Assignment
+{
+    private string _title;
+
+    public WritingAssignment(string studentName, string topic, string title) : base(studentName, topic)
+    {
+        _title = title;
+    }
+
+    public string GetWritingInformation()
+    {
+        return $"{GetSummary()} -Title: {_title},";
+    }
+}
