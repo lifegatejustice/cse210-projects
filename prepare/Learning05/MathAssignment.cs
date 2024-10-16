3f8677241496f8982db6201302c8ec5013a93fc1
lifegatejustice <lifegatedetom@gmail.com>
2024-10-16 02:32:36 -0700

    "Added MathAssignment class and updated Program.cs to use it"

--- prepare/Learning05/MathAssignment.cs
+++ prepare/Learning05/MathAssignment.cs
@@ -1,4 +1,22 @@
 using System;
 using System.Collections;
 
-public class
+public class MathAssignment : Assignment
+{
+   private string _textbookSection;
+   private string _problems;
+
+   public MathAssignment(string studentName, string topic, string textbookSection, string problems) : base(studentName, topic)
+
+   {
+        _textbookSection = textbookSection;
+        _problems = problems;
+   }
+
+   public string GetHomeworkList()
+   {
+      return  "Homework List for " + _textbookSection + ":\n" + _problems;
+   }
+
+
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,7 +5,8 @@ class Program
     static void Main(string[] args)
     {
         //Create New Instance of the class
-        Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
-        Console.WriteLine(assignment.GetSummary());
+        MathAssignment mathAssignment = new MathAssignment("Samuel Bennett", "Multiplication", "Section 7.3", "Problems 8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
     }
 }
