b3e32ff43f97b3eb0924dd501a97721be0e9c416
lifegatejustice <lifegatedetom@gmail.com>
2024-10-16 03:46:47 -0700

    "Updated WritingAssignment constructor parameters and GetWritingInformation method return string in Program.cs and WritingAssignment.cs"

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         //Create New Instance of the class
-        WritingAssignment writingAssignment = new WritingAssignment("Samuel Bennett", "Multiplication", "The Causes of World War II by Mary Waters");
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II by Mary Waters");
         Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
--- prepare/Learning05/WritingAssignment.cs
+++ prepare/Learning05/WritingAssignment.cs
@@ -12,6 +12,6 @@ public class WritingAssignment : Assignment
 
     public string GetWritingInformation()
     {
-        return $"{GetSummary()}, \r\n{_title},";
+        return $"{GetSummary()} \r\n{_title}.";
     }
 }
