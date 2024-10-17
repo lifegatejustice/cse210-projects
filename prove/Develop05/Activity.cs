cc22d995b58d903d6536803729df1a4272d6261a
lifegatejustice <lifegatedetom@gmail.com>
2024-10-17 04:12:19 -0700

    "Removed console output for menu options and added empty DisplayEndingMessage method"

--- prove/Develop05/Activity.cs
+++ prove/Develop05/Activity.cs
@@ -16,14 +16,13 @@ public class Activity
 
     public void DisplayStartingMessage()
     {
-        Console.WriteLine("Menu Options");
-        Console.WriteLine(" 1. Start breathing activity");
-        Console.WriteLine(" 2. Start reflecting activity");
-        Console.WriteLine(" 1. Start listing activity");
-        Console.WriteLine(" 1. Quit");
-        Console.ReadLine();
-        Console.WriteLine("Select a choice from the menu: ");
 
-        
+
+
+    }
+
+    public void DisplayEndingMessage()
+    {
+
     }
 }
