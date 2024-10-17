5b955c928f26bc15aa266e2dd407b6384b9b5288
lifegatejustice <lifegatedetom@gmail.com>
2024-10-17 07:21:33 -0700

    "Added inheritance from 'Activity' class and a constructor to ReflectingActivity class."

--- prove/Develop05/ReflectingActivity.cs
+++ prove/Develop05/ReflectingActivity.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using  System.Collections;
 
-public class ReflectingActivity
+public class ReflectingActivity : Activity
 {
    private List<string> _prompts = new List<string> { "Think of a time when you stood up for someone else.", "Think of a time when you did something really difficult.", "Think of a time when you helped someone in need.", "Think of a time when you did something truly selfless." };
-    private List<string> _questions = new List<string> { "Why was that experience meaningful to you?", "Have you ever done anything like this before?", "How did you get started?", "How did you feel when it was complete?", "What made this time different than other times when you were not as successful?", "What is your favorite thing about this experience?", "What could you learn from this experience that applies to other situations?", "What did you learn about yourself through this experience?", "How can you keep this experience in mind in the future?"};
+   private List<string> _questions = new List<string> { "Why was that experience meaningful to you?", "Have you ever done anything like this before?", "How did you get started?", "How did you feel when it was complete?", "What made this time different than other times when you were not as successful?", "What is your favorite thing about this experience?", "What could you learn from this experience that applies to other situations?", "What did you learn about yourself through this experience?", "How can you keep this experience in mind in the future?"};
+   
+   public ReflectingActivity() : 
 
 }
